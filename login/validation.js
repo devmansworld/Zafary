ea981247aaf1f84100f7dbf0176dcc0cdecae4c5
Leo Pacheco <leonardopachecoduran@gmail.com>
2019-12-24 02:01:02 -0300

    Add files via upload

---
+++ login/validation.js
@@ -0,0 +1,11 @@
+function comparar()
+{
+     if($("#correo").val()=="ejemplo@gmail.com" &&
+$("#contrasenia").val()=="123456"){
+    alert("Iniciando Sesión")
+}
+else{
+
+    alert("Email y/o contraseña incorrectos")
+}
+}
